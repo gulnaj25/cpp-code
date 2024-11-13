bdef22eedc04ccda78fe23427b782df59a285839
gulnaj25 <gulnajkhan525@gmail.com>
2024-11-13 17:07:56 +0530

    inline.cpp

--- inlinefxn.cpp
+++ inlinefxn.cpp
@@ -1,4 +1,4 @@
-/*#include<iostream>
+#include<iostream>
 using namespace std;
 
  inline int product(int a,int b){//we use inline when code repeat and repeat and it is only for small fxn 
@@ -25,17 +25,17 @@ int main()
     cout<<"the product of a and b is"<<product(a,b);
     cout<<"the product of a and b is"<<product(a,b);
  return 0;
-}*/
+}
 
 
 
 //STATIC fxn
-
+                 //inline and static ,both never use in same code
 include<iostream>
 using namespace std;
 
 int product(int a,int b){
-    static c;
+    static c;//after giving one value the line never consider again
     c=c+1;
     return a*b+c;
 }
@@ -45,5 +45,11 @@ int main(){
     cin>>a>>b;
     cout<<"the product of a and b is"<<product(a,b);
     cout<<"the product of a and b is"<<product(a,b);
+    cout<<"the product of a and b is"<<product(a,b);
+    cout<<"the product of a and b is"<<product(a,b);
      return 0;
-}
+}    /* o/p- when a=2,b=3
+     7
+     8
+     9
+     10  */
